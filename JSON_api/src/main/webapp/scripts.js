6cebbdc5046c154b9a71804cc998b72c94f22b73
bentom08 <bentom06@hotmail.co.uk>
2018-10-25 17:31:52 +0100

    api web page update

--- JSON_api/src/main/webapp/scripts.js
+++ JSON_api/src/main/webapp/scripts.js
@@ -58,5 +58,22 @@ function addMovie() {
 function displayJSON(x) {
 		var json = JSON.parse(x);
 
-		document.getElementById("Output").innerHTML = "id: " + json.id + "\nTitle: " + json.title  + "\nGenre: " + json.genre + "\nAge Rating: " + json.ageRating;
+		document.getElementById("Output").innerHTML = "id: " + json.id + "<br>Title: " + json.title  + "<br>Genre: " + json.genre + "<br>Age Rating: " + json.ageRating;
+}
+
+function getTable(myurl) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", myurl, false);
+	xmlHttp.send(null);
+	var n = xmlHttp.response.length;
+	var string = '';
+	var array = eval(xmlHttp.response);
+
+	for (var i = 0; i < n; i++) {
+		var row = array[i];
+		var json = JSON.parse('"'  + row + '"');
+		string += "<tr><td>" + json.id + "</td><td>" + json.title + "</td><td>" + json.genre + "</td><td>" + json.ageRating + "</td></tr>";
+	}
+
+	document.getElementById("Table").innerHTML = "<th>ID</th><th>Title</th><th>Genre</th><th>Age Rating</th>" + string;
 }
